b428389a8ed00cf19dc3ffa20ef966a5e9cc479d
Carlos Irvin <irvin.aparicio@uabc.edu.mx>
2017-11-21 09:08:39 -0800

    Cambie el HTML para el sitio.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h()zv#q2*qlz=ebb-2qa!&+x^xxw7&rr(z%$qdai&%*bwwbunm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["Chihua.pythonanywhere.com", "127.0.0.1"]
 
 
 # Application definition
